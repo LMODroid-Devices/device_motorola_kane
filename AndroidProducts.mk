29a8432c75aaa19fb0537bce24cc58f249158676
LMO GerritBot <gerrit@libremobileos.com>
2023-04-21 22:24:41 +0000

    Adapt to LMODroid by device-add script
    Change-Id: Ia91da78fe21e3a2e7bd6aa676d436cfceaa56226

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_kane.mk
+    $(LOCAL_DIR)/lmodroid_kane.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_kane-user \
-    lineage_kane-userdebug \
-    lineage_kane-eng
+    lmodroid_kane-user \
+    lmodroid_kane-userdebug \
+    lmodroid_kane-eng
